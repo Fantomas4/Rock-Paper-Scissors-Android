c19cae093699d9b7d7857088def32964aa9973e5
Fantomas4 <stefanokf@auth.gr>
2019-05-30 06:09:01 +0300

    Made minor changes to PlayerChoice.java. Fixed a bug where the "NONE" player choice would be randomly picked as the bot's random choice, causing inconsistent application behavior.

--- app/src/main/java/com/example/rockpaperscissors/PlayerChoice.java
+++ app/src/main/java/com/example/rockpaperscissors/PlayerChoice.java
@@ -10,6 +10,8 @@ public enum PlayerChoice {
 
     public static PlayerChoice getRandomChoice() {
         Random random = new Random();
-        return values()[random.nextInt(values().length)];
+        // A random choice is picked from ROCK, PAPER, SCISSORS only!
+        // NONE should NOT be picked as a random bot choice!
+        return values()[random.nextInt(values().length-1)];
     }
 }
